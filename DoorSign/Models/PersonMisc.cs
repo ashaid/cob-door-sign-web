abe8e8f491fbf7c71692b2aba45a726829486796
Anthony Shaidaee <ashaid2@lsu.edu>
2020-08-18 13:22:53 -0500

    fixed blank

--- DoorSign/Models/PersonMisc.cs
+++ DoorSign/Models/PersonMisc.cs
@@ -5,11 +5,20 @@ using System.Threading.Tasks;
 
 namespace DoorSign.Models
 {
-    public class PersonMisc : Person
+    public class PersonMisc 
     {
         public string? Heading { get; set; }
 
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Department { get; set; }
         public string? RText { get; set; }
+
+        public string? Title { get; set; }
+
+        public int? RoomNumber { get; set; }
 
         public int? RN { get; set; }
 
